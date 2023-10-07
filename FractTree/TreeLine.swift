22dda3285c3a3756f957cb59245d7a07d143517b
LUIS FELIPE B PEREIRA <luisfelipebpereira@LUISs-MacBook-Air.local>
2023-10-07 03:30:21 -0300

    initial

---
+++ FractTree/TreeLine.swift
@@ -0,0 +1,84 @@
+//
+//  TreeLine.swift
+//  FractTree
+//
+//  Created by LUIS FELIPE B PEREIRA on 07/10/23.
+//
+
+import Foundation
+import UIKit
+
+class TreeLine  {
+    var origin: CGPoint = .zero
+    var end: CGPoint = .zero
+    var length: CGFloat = 0.0
+    var angle: CGFloat = 0.0
+    var shape = CAShapeLayer()
+    var animation = CABasicAnimation(keyPath: "strokeEnd")
+    
+    init(origin: CGPoint, end: CGPoint) {
+        self.origin = origin
+        self.end = end
+        shape.fillColor = UIColor.clear.cgColor
+    }
+    
+    init(origin: CGPoint, length: CGFloat, angle: CGFloat) {
+        self.origin = origin
+        self.length = length
+        self.angle = angle
+        shape.fillColor = UIColor.clear.cgColor
+        end.x = origin.x + length*cos(angle)
+        end.y = origin.y + length*sin(angle)
+    }
+    
+    func draw(inside view: UIView) {
+        
+        let path = makePath()
+        shape = CAShapeLayer()
+        shape.path = path.cgPath
+        setLineColor(.black)
+        setLineWidth(2)
+        animate()
+        view.layer.addSublayer(shape)
+    }
+    
+    func setLineColor(_ color: UIColor) {
+        shape.strokeColor = color.cgColor
+    }
+    
+    func setLineWidth(_ w: CGFloat) {
+        shape.lineWidth = w
+    }
+    
+    func createRightLine() -> TreeLine {
+        let origin = self.end
+        let length = self.length*0.8
+        let angle = self.angle - Double.pi/6
+        let line =  TreeLine(origin: origin, length: length, angle: angle)
+        return line
+    }
+    
+    func createLeftLine() -> TreeLine {
+        let origin = self.end
+        let length = self.length*0.8
+        let angle = self.angle + Double.pi/6
+        let line =  TreeLine(origin: origin, length: length, angle: angle)
+        return line
+    }
+    
+    private func animate () {
+        animation.fromValue = 0.0
+        animation.toValue = 1.0
+        animation.duration = 3 * length/250
+        shape.add(animation, forKey: "lineAnimation")
+    }
+    
+    private func makePath() -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: origin)
+        path.addLine(to: end)
+        path.close()
+        return path
+    }
+    
+}
---
+++ FractTree/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  FractTree
+//
+//  Created by LUIS FELIPE B PEREIRA on 21/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, CAAnimationDelegate {
+    
+    var currentLevel: [TreeLine] = []
+    var nextLevel: [TreeLine] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let root = TreeLine(origin: .init(x: view.frame.midX, y: .zero), length: 100, angle: Double.pi/2)
+        currentLevel.append(root)
+        drawTree()
+        
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        currentLevel = nextLevel
+        nextLevel = []
+        drawTree()
+    }
+    
+    private func drawTree() {
+        var isFirstFromLevel = true
+        while !currentLevel.isEmpty{
+            let line = currentLevel.removeFirst()
+            if isFirstFromLevel {
+                line.animation.delegate = self
+                isFirstFromLevel = false
+            }
+            line.draw(inside: view)
+            let right = line.createRightLine()
+            let left = line.createLeftLine()
+            if right.length > 5.0 {
+                nextLevel.append(right)
+                nextLevel.append(left)
+            }
+            
+        }
+        
+    }
+    
+}
+
+
